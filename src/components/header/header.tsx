29a3207654e6daa2e9fa5fe69de56aaf7e6bfc19
Icaro Gandine <icarog28@gmail.com>
2025-06-10 15:57:48 -0300

    adds add and search routes

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,7 @@
 // src/components/Header.tsx
 import { Link, useNavigate } from "react-router-dom";
 import { useAddressStore } from "../../stores/addresses";
+import { PlusOutlined, SearchOutlined } from "@ant-design/icons";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -15,9 +16,26 @@ const Header = () => {
   return (
     <header className="bg-cardBg text-white shadow-md">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
-        <Link to="/add" className="text-xl font-bold flex items-center">
-          Agenda de endereços
-        </Link>
+        <div className="flex gap-6">
+          <Link to="/add" className="text-xl font-bold flex items-center">
+            Agenda de endereços
+          </Link>
+          <div className="flex gap-4">
+            <Link
+              to="/add"
+              className="px-4 py-2 bg-buttonBg rounded-lg hover:bg-buttonHover transition-colors flex items-center gap-2"
+            >
+              <PlusOutlined />
+            </Link>
+
+            <Link
+              to="/search"
+              className="px-4 py-2 bg-buttonBg rounded-lg hover:bg-buttonHover transition-colors flex items-center gap-2"
+            >
+              <SearchOutlined />
+            </Link>
+          </div>
+        </div>
 
         <div className="flex items-center space-x-6">
           <div className="flex whitespace-nowrap w-full">
@@ -26,7 +44,7 @@ const Header = () => {
 
           <button
             onClick={handleLogout}
-            className="block w-full text-left px-4 py-2 bg-buttonBg rounded-lg cursor-pointer"
+            className="block w-full text-left px-4 py-2 bg-buttonBg rounded-lg cursor-pointer hover:bg-buttonHover transition-colors"
           >
             Sair
           </button>
